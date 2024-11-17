15f2f3f3ba269d25231cf8c9a5319433bd0f08fe
Denis Semenov <denis.semenov@pix.ru>
2024-11-17 14:32:30 +0300

    Stage

--- HockServerRunner/Program.cs
+++ HockServerRunner/Program.cs
@@ -1,4 +1,4 @@
 ﻿
 var server = new HQMServer();
-await server.RunServer(27585, "https://sam2.github.io/HQMMasterServerEndpoint/", new HQMServerConfiguration());
+await server.RunServer(27599, "https://sam2.github.io/HQMMasterServerEndpoint/", new HQMServerConfiguration());
 
